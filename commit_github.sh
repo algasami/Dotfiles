4bead96a3473d951dd9803c035070e52d12f7569
Algasami <105358814+algasami@users.noreply.github.com>
2022-08-14 18:37:06 +0800

    Automatic Procedure :robot:

---
+++ commit_github.sh
@@ -0,0 +1,7 @@
+cp ~/.config/kitty/kitty.conf ./kitty/kitty.conf
+cp ~/.config/fish/config.fish ./fish/config.fish
+cp ~/.i3/config ./i3/config
+cp ~/.Xresources ./urxvt/.Xresources
+cp ~/.config/neofetch/config.conf ./neofetch/config.conf
+cp ~/.vimrc ./vim/.vimrc
+cp ~/.bashrc ./bash/.bashrc
---
+++ use_github.sh
@@ -0,0 +1,7 @@
+sudo cp ./kitty/kitty.conf ~/.config/kitty/kitty.conf
+sudo cp ./fish/config.fish ~/.config/fish/config.fish 
+sudo cp ./i3/config ~/.i3/config 
+sudo cp ./urxvt/.Xresources ~/.Xresources
+sudo cp ./neofetch/config.conf ~/.config/neofetch/config.conf
+sudo cp ./vim/.vimrc ~/.vimrc 
+sudo cp ./bash/.bashrc ~/.bashrc 
