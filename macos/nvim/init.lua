5c789c17948f3dc5cc1d33be531bbf1ffae5cfb9
algasami <105358814+algasami@users.noreply.github.com>
2023-06-21 22:42:25 +0800

    Changed some init in nvim

--- macos/nvim/init.lua
+++ macos/nvim/init.lua
@@ -28,6 +28,18 @@ cmd('filetype plugin indent on')
 cmd('syntax on')
 cmd('colorscheme kanagawa')
 
+cmd([[
+augroup fmt
+autocmd!
+autocmd BufWritePre * undojoin | Neoformat
+augroup END]])
+
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+
 require('lspkind').init({
     -- DEPRECATED (use mode instead): enables text annotations
     --
@@ -50,30 +62,30 @@ require('lspkind').init({
     --
     -- default: {}
     symbol_map = {
-      Text = "󰉿",
-      Method = "󰆧",
-      Function = "󰊕",
-      Constructor = "",
-      Field = "󰜢",
-      Variable = "󰀫",
-      Class = "󰠱",
-      Interface = "",
-      Module = "",
-      Property = "󰜢",
-      Unit = "󰑭",
-      Value = "󰎠",
-      Enum = "",
-      Keyword = "󰌋",
-      Snippet = "",
-      Color = "󰏘",
-      File = "󰈙",
-      Reference = "󰈇",
-      Folder = "󰉋",
-      EnumMember = "",
-      Constant = "󰏿",
-      Struct = "󰙅",
-      Event = "",
-      Operator = "󰆕",
-      TypeParameter = "",
+        Text = "󰉿",
+        Method = "󰆧",
+        Function = "󰊕",
+        Constructor = "",
+        Field = "󰜢",
+        Variable = "󰀫",
+        Class = "󰠱",
+        Interface = "",
+        Module = "",
+        Property = "󰜢",
+        Unit = "󰑭",
+        Value = "󰎠",
+        Enum = "",
+        Keyword = "󰌋",
+        Snippet = "",
+        Color = "󰏘",
+        File = "󰈙",
+        Reference = "󰈇",
+        Folder = "󰉋",
+        EnumMember = "",
+        Constant = "󰏿",
+        Struct = "󰙅",
+        Event = "",
+        Operator = "󰆕",
+        TypeParameter = "",
     },
 })
--- macos/nvim/lua/plugins.lua
+++ macos/nvim/lua/plugins.lua
@@ -4,14 +4,21 @@
 -- vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-    -- Packer can manage itself
-    use 'wbthomason/packer.nvim'
+  -- Packer can manage itself
+  use 'wbthomason/packer.nvim'
 
-    use 'rebelot/kanagawa.nvim' -- nice theme!
+  use 'rebelot/kanagawa.nvim' -- nice theme!
+  use 'sbdchd/neoformat' -- format
 
-    use 'preservim/nerdtree' -- tree
-    use 'ryanoasis/vim-devicons' -- icons
+  use 'preservim/nerdtree' -- tree
 
-    use 'onsails/lspkind.nvim' -- vscode-like completion icons
-    use {'neoclide/coc.nvim', branch = "release"} -- autocomplete backend (LSP)
+  use 'ryanoasis/vim-devicons' -- icons
+
+  use {
+    'nvim-telescope/telescope.nvim', tag = '0.1.1',
+    requires = { {'nvim-lua/plenary.nvim'} }
+  }
+
+  use 'onsails/lspkind.nvim' -- vscode-like completion icons
+  use {'neoclide/coc.nvim', branch = "release"} -- autocomplete backend (LSP)
 end)
